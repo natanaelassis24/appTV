954978ba3e990548233d12e8b02ec290eabf2f20
natanael <nataanaelassis77@gmail.com>
2026-03-22 14:42:45 -0300

    0.-3

--- src/channels.js
+++ src/channels.js
@@ -53,9 +53,509 @@ export const CHANNELS = [
 {
   name: 'Cartoon Network',
   url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/cartoonnetwork.m3u8',
-  number: '0',
+  number: '5',
   category: 'Canais',
   logoImage: 'https://yt3.googleusercontent.com/K4BGYVFPK9QWZiLLP49cRkxl-QtR8gLvmjLTkiRS1e97fnrAfdmIjdFj4mmIgarHySptEdAs=s160-c-k-c0x00ffffff-no-rj',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Discovery Kids',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/discoverykids.m3u8',
+  number: '6',
+  category: 'Canais',
+  logoImage: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/afaaa8ee-bea2-45b3-9bb4-7096c9802384/dfr9d72-eba46bad-4916-4f69-981d-e4f948d8eb6c.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Cinemax',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/cinemax.m3u8',
+  number: '7',
+  category: 'Canais',
+  logoImage: 'https://download.logo.wine/logo/Cinemax/Cinemax-Logo.wine.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'AXN',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/axn.m3u8',
+  number: '8',
+  category: 'Canais',
+  logoImage: 'https://cdn.telaviva.com.br/wp-content/uploads/2016/02/Novo-logo-AXN.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Band',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/band.m3u8',
+  number: '9',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/4/4d/Band_Marca.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'CNN Brasil',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/cnnbrasil.m3u8',
+  number: '10',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/CNN_International_logo.svg/3840px-CNN_International_logo.svg.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Combate',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/combate.m3u8',
+  number: '11',
+  category: 'Canais',
+  logoImage: 'https://sua-logo.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'A&E',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/ae.m3u8',
+  number: '12',
+  category: 'Canais',
+  logoImage: 'https://images.seeklogo.com/logo-png/21/1/ae-network-logo-png_seeklogo-219139.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Animal Planet',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/animalplanet.m3u8',
+  number: '13',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/2018_Animal_Planet_logo.svg/960px-2018_Animal_Planet_logo.svg.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Comedy Central',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/comedycentral.m3u8',
+  number: '14',
+  category: 'Canais',
+  logoImage: 'https://logodownload.org/wp-content/uploads/2021/05/comedy-central-logo-0.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Discovery Channel',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/discoverychannel.m3u8',
+  number: '15',
+  category: 'Canais',
+  logoImage: 'https://logodownload.org/wp-content/uploads/2017/04/discovery-channel-logo-0.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Discovery H&H',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/discoveryhh.m3u8',
+  number: '16',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/e/e5/Discovery_h%26h.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Discovery ID',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/discoveryid.m3u8',
+  number: '17',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/0/0b/ID_-_Investiga%C3%A7%C3%A3o_Discovery.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Discovery Science',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/discoveryscience.m3u8',
+  number: '18',
+  category: 'Canais',
+  logoImage: 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/082015/discovery-science.png?itok=NDlWi00F',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Discovery Theater',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/discoverytheather.m3u8',
+  number: '19',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/2015_Discovery_theater_HD_logo.svg/120px-2015_Discovery_theater_HD_logo.svg.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Discovery Turbo',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/discoveryturbo.m3u8',
+  number: '20',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/1/11/Discovery_Turbo.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Espn',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/espn.m3u8',
+  number: '21',
+  category: 'Canais',
+  logoImage: 'https://a1.espncdn.com/combiner/i?img=%2Fi%2Fespn%2Fespn_logos%2Fespn_red.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Espn 2',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/espn2.m3u8',
+  number: '22',
+  category: 'Canais',
+  logoImage: 'https://i.pinimg.com/736x/97/d5/81/97d581566431fe83e4d8f390424b9987.jpg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Food Network',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/foodnetwork.m3u8',
+  number: '23',
+  category: 'Canais',
+  logoImage: 'https://logos-world.net/wp-content/uploads/2023/05/Food-Network-Logo-2003.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Gloob',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/gloob.m3u8',
+  number: '24',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Gloob_logo.svg/1280px-Gloob_logo.svg.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'HBO',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/hbo.m3u8',
+  number: '25',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/d/de/HBO_logo.svg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'HBO 2',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/hbo2.m3u8',
+  number: '26',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/6/6c/HBO2_logo.svg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'HBO Family',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/hbofamily.m3u8',
+  number: '27',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/HBO_Family_logo.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'HBO Mundi',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/hbomundi.m3u8',
+  number: '28',
+  category: 'Canais',
+  logoImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbAXp130AYAc4pXI_m-PqxrYip5VQCNRfUVA&s',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'HBO Plus',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/hboplus.m3u8',
+  number: '29',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/HBO_Plus.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'HBO POP',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/hbopop.m3u8',
+  number: '30',
+  category: 'Canais',
+  logoImage: 'https://tvmap.com.br/images/ch/665.jpg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'HBO Xtreme',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/hboxtreme.m3u8',
+  number: '31',
+  category: 'Canais',
+  logoImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyfzy_zryHCA8eh6XVhjEB3jjVQZ5W4qUmRA&s',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'History',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/history.m3u8',
+  number: '32',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/History_Logo.svg/960px-History_Logo.svg.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'History 2',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/history2.m3u8',
+  number: '33',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/History2Logo2019.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Megapix',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/megapix.m3u8',
+  number: '34',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/Megapix_logo_2011.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'MTV',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/mtv.m3u8',
+  number: '35',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/6/68/MTV_2021_%28brand_version%29.svg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Multishow',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/multishow.m3u8',
+  number: '36',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Multishow_logo_red_2012.svg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'OFF',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/off.m3u8',
+  number: '37',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/e/ee/Logo_Canal_OFF.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Sony Channel',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/sonychannel.m3u8',
+  number: '38',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Sony_Channel_Logo_2019.svg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Space',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/space.m3u8',
+  number: '39',
+  category: 'Canais',
+  logoImage: 'https://logos-world.net/wp-content/uploads/2023/03/Space-Logo.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'PT – Sporttv 1',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/pt_sportv1.m3u8',
+  number: '40',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/2/2e/Sport_TV1_%282023%29.svg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'PT – Sporttv 3',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/pt_sportv3.m3u8',
+  number: '41',
+  category: 'Canais',
+  logoImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcmyMFb2fSC-c_ngRvLBnUzF-nxgyfVES3Nw&s',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'PT – Sporttv 2',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/pt_sportv2.m3u8',
+  number: '42',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/2/20/Sport_TV2_%282023%29.svg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'PT – Sporttv 4',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/pt_sportv4.m3u8',
+  number: '43',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Sport_TV4_%282023%29.svg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'TCM',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/tcm.m3u8',
+  number: '44',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Turner_Classic_Movies_%28TCM%2C_Latin_America%29_-_2015_logo.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Telecine Action',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/telecineaction.m3u8',
+  number: '45',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Telecine_Action.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Telecine Cult',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/telecinecult.m3u8',
+  number: '46',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/3/3b/Telecine_Cult.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Telecine Fun',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/telecinefun.m3u8',
+  number: '47',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Telecine_Fun_2.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Telecine Pipoca',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/telecinepipoca.m3u8',
+  number: '48',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/2/2a/Telecine_Pipoca.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Telecine Premium',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/telecinepremium.m3u8',
+  number: '49',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Telecine_Premium.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Telecine Touch',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/telecinetouch.m3u8',
+  number: '50',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Telecine_Touch.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'TNT',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/tnt.m3u8',
+  number: '51',
+  category: 'Canais',
+  logoImage: 'https://sua-logo.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'TNT Novelas',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/tntnovelas.m3u8',
+  number: '52',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Logo_TNT_Series.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'TNT Series',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/tntseries.m3u8',
+  number: '53',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/8/86/TNT_Series.png',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Universal',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/universaltv.m3u8',
+  number: '54',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Universal_Pictures_logo.svg',
+  logo: 'NC',
+  sourceType: 'hls',
+  description: 'Descricao curta do canal.'
+},
+{
+  name: 'Warnerchannel',
+  url: 'https://streamverde.s27-usa-cloudfront-net.online/fontes/streamverde/warnerchannel.m3u8',
+  number: '55',
+  category: 'Canais',
+  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Warner_Bros_logo.svg/500px-Warner_Bros_logo.svg.png',
   logo: 'NC',
   sourceType: 'hls',
   description: 'Descricao curta do canal.'
